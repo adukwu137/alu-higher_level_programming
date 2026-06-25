5056e0b05eb935f4b0736773d0a1f6e313e60d57
adukwu137 <o.adukwu@alustudent.com>
2026-06-25 16:27:53 -0700

    fix: resolve string literals restriction using exact space slices

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-print(f"{str[39:67]} {str[106:110]} {str[0:6]}")
+print(str[39:67] + str[67] + str[106:110] + str[105] + str[0:6])

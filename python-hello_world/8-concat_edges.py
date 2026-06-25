3bfde4bd6253f7a68339e8468a2b0fee4111ab24
adukwu137 <o.adukwu@alustudent.com>
2026-06-25 16:57:08 -0700

    fix: adjust slice indices for exact character matching

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming language that combines remarkable power with very clear syntax"
-str = str[39:67] + str[67] + str[106:110] + str[105] + str[0:6]
+str = str[39:67] + str[107:111] + str[111] + str[0:6]
 print(str)

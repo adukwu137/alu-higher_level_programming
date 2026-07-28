c7448d55f501dc03f4b73d0700dd6b7e20581a1e
adukwu137 <o.adukwu@alustudent.com>
2026-07-28 12:20:44 -0700

    Fix docstring, PEP8 formatting, and output for 0-hbtn_status.py

--- python-network_1/0-hbtn_status.py
+++ python-network_1/0-hbtn_status.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
-"""Fetches https://alu-intranet.hbtn.io/status using urllib"""
+"""
+Python script that fetches https://alu-intranet.hbtn.io/status
+using the urllib package.
+"""
 import urllib.request
 
 
 if __name__ == "__main__":
-    url = "https://alu-intranet.hbtn.io/status"
-    req = urllib.request.Request(url)
+    req = urllib.request.Request('https://alu-intranet.hbtn.io/status')
     with urllib.request.urlopen(req) as response:
-        content = response.read()
+        body = response.read()
         print("Body response:")
-        print(f"\t- type: {type(content)}")
-        print(f"\t- content: {content}")
-        print(f"\t- utf8 content: {content.decode('utf-8')}")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode('utf-8')))

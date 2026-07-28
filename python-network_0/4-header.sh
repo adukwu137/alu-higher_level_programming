3105c3672cb96ac3afb8d744d409ca50ec6e7635
adukwu137 <o.adukwu@alustudent.com>
2026-07-28 11:59:41 -0700

    Fix headers for Task 4

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the URL with header X-HolbertonSchool-User-Id: 98
-curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to the URL with the required user ID header
+curl -s -H "X-HolbertonSchool-User-Id: 98" -H "X-School-User-Id: 98" "$1"

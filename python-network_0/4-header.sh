e200100b027194349abb34e9000f3aae0d6eb796
adukwu137 <o.adukwu@alustudent.com>
2026-07-28 11:57:40 -0700

    Fix 4-header.sh curl flags order

--- python-network_0/4-header.sh
+++ python-network_0/4-header.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a GET request with a header X-HolbertonSchool-User-Id set to 98
-curl -sH "X-HolbertonSchool-User-Id: 98" "$1"
+# Sends a GET request to the URL with header X-HolbertonSchool-User-Id: 98
+curl -s -H "X-HolbertonSchool-User-Id: 98" "$1"
